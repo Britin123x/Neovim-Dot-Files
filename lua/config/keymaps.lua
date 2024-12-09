6ad1ef1ffad00f3d62f14ca90f61fe5e2a42475b
Britin Hanna <britinhanna@outlook.com>
2024-12-09 14:03:23 -0600

    This is my new neovim config with LazyVim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", "<leader>ft", function()
+  Snacks.terminal(nil, { cwd = vim.fn.expand("%:p:h") })
+end, { desc = "Terminal (Terminal under File)" })
